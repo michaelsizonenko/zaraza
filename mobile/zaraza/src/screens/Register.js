8a51cee2049545ae90b44dc0756e77cf047ccabd
michaelsizonenko <michael.sizonenko.17@gmail.com>
2020-04-01 16:14:42 +0300

    register form

--- mobile/zaraza/src/screens/Register.js
+++ mobile/zaraza/src/screens/Register.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, Button, Text, StyleSheet} from 'react-native';
+import {View, Button, Text, StyleSheet, SafeAreaView, Alert} from 'react-native';
 
 
 export default class RegisterScreen extends React.Component {
@@ -7,32 +7,39 @@ export default class RegisterScreen extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <View>
-                    <Text>Register form</Text>
-                    <Button title={"Register"} />
-                </View>
+
+                <SafeAreaView style={styles.container}>
+                    <View>
+                        <Text style={styles.title}>Register form</Text>
+                        <Button
+                            title={"Register"}
+                            onPress={() => Alert.alert('Simple Button pressed')}
+                        />
+                    </View>
+                </SafeAreaView>
             </React.Fragment>
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-    marginHorizontal: 16,
-  },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
+    container: {
+        flex: 1,
+        marginTop: 10,
+        marginHorizontal: 16,
+    },
+    title: {
+        textAlign: 'center',
+        marginVertical: 8,
+        fontSize: 20
+    },
+    fixToText: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    separator: {
+        marginVertical: 8,
+        borderBottomColor: '#737373',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
 });
