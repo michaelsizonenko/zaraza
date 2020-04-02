f43783d6c46d439caa2dcffb622f04d86f3213f7
michaelsizonenko <michael.sizonenko.17@gmail.com>
2020-04-02 15:07:25 +0300

    update phone number field

--- zaraza/citizens/models.py
+++ zaraza/citizens/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 GENDER_CHOICES = (('M', 'Man'), ('W', 'Woman'))
@@ -11,7 +12,7 @@ class Citizen(models.Model):
     first_name = models.CharField(max_length=100)
     second_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=12, unique=True)
+    phone_number = PhoneNumberField(unique=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     birth_date = models.DateField()
     location = models.CharField(max_length=100)
