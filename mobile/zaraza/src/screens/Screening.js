66427914dab0823b61e1cdf5575ddd06cbc9ee16
michaelsizonenko <michael.sizonenko.17@gmail.com>
2020-04-09 16:19:26 +0300

    minor fix

--- mobile/zaraza/src/screens/Screening.js
+++ mobile/zaraza/src/screens/Screening.js
@@ -1,25 +1,24 @@
 import * as React from 'react'
-import { Button, FlatList, SafeAreaView, ScrollView, Text, View } from 'react-native'
-import { Formik } from 'formik'
-import { styles } from '../styles/Styles'
-import { ValidatedTextInput } from '../components/ValidatedInput'
+import {Button, FlatList, SafeAreaView, ScrollView, Text, View} from 'react-native'
+import {Formik} from 'formik'
+import {styles} from '../styles/Styles'
+import {ValidatedTextInput} from '../components/ValidatedInput'
 import * as Yup from 'yup'
-import { translate } from '../config/AppConfig'
-import { IconButton } from 'react-native-paper'
+import {translate} from '../config/AppConfig'
+import {IconButton} from 'react-native-paper'
 
 
+function SearchItem(props) {
+    let person = props.person;
+    this.toggleTemperature = (instance) => {
+        instance.showTemperature = !instance.showTemperature;
+    };
+    this.postTemperature = () => {
+    };
 
-  function SearchItem (props) {
-      let person = props.person;
-      this.toggleTemperature = (instance) => {
-          instance.showTemperature = !instance.showTemperature;
-      }
-      this.postTemperature =()=>
-      {}
-
-      return (
+    return (
         <Formik style={styles.searchItemContainer}
-                onSubmit={this.postTemperature} initialValues={{ id: person.id }}>
+                onSubmit={this.postTemperature} initialValues={{id: person.id}}>
             <Text>{person.first_name}  </Text>
             <Text>{person.second_name} </Text>
             <Text>{person.last_name}   </Text>
@@ -31,70 +30,68 @@ import { IconButton } from 'react-native-paper'
             <Text>{person.address}     </Text>
             <IconButton name='thermometer' onClick={this.toggleTemperature.bind(this, person)}/>
             {person.showTemperature && (
-              <>
-                  <ValidatedTextInput name='temperature' placeholder={translate('Citizen\'s temperature')} {...props}/>
-                  <IconButton name='submit' onPress={this.submitTemperature.bind(this,person)}/>
-              </>)}
+                <>
+                    <ValidatedTextInput name='temperature'
+                                        placeholder={translate('Citizen\'s temperature')} {...props}/>
+                    <IconButton name='submit' onPress={this.submitTemperature.bind(this, person)}/>
+                </>)}
         </Formik>);
 
-  }
+}
 
-function SearchResult (props) {
+function SearchResult(props) {
 
-    if (!props.searchItems){
-        return   <Text>{translate("Run search to see locate citizen...")}</Text>;
+    if (!props.searchItems) {
+        return <Text>{translate("Run search to see locate citizen...")}</Text>;
     }
 
-    return
-
-  (
-    <FlatList
-      scrollEnabled={true}
-      data={props.searchItems}
-      renderItem={({ item }) => <SearchItem person={item}{...person}/>}
-      keyExtractor={item => item.id}
-    />)
+    return (
+        <FlatList
+            scrollEnabled={true}
+            data={props.searchItems}
+            renderItem={({item}) => <SearchItem person={item}{...person}/>}
+            keyExtractor={item => item.id}
+        />)
 }
 
 export default class ScreeningScreen extends React.Component {
 
-  ScreeningSchema = Yup.object().shape({})
+    ScreeningSchema = Yup.object().shape({});
 
-  render () {
-    return (
-      <SafeAreaView style={styles.container}>
-        <ScrollView>
-          <Formik
-            initialValues={{}}
-            onSubmit={this._onSubmit}
-            validationSchema={this.ScreeningSchema}
-          >
-            {(props) => (
-              <View style={styles.form}>
-                <View style={[styles.horizontal, styles.row]}>
-                  <ValidatedTextInput name='query'
-                                      {...props} style={styles.searchQuery}/>
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <ScrollView>
+                    <Formik
+                        initialValues={{}}
+                        onSubmit={this._onSubmit}
+                        validationSchema={this.ScreeningSchema}
+                    >
+                        {(props) => (
+                            <View style={styles.form}>
+                                <View style={[styles.horizontal, styles.row]}>
+                                    <ValidatedTextInput name='query'
+                                                        {...props} style={styles.searchQuery}/>
 
-                  <IconButton icon='database-search' style={styles.searchButton}/>
-                </View>
-                <SearchResult/>
-
-
+                                    <IconButton icon='database-search' style={styles.searchButton}/>
+                                </View>
+                                <SearchResult/>
 
 
-                {/*<Text>{"Errors : " + JSON.stringify(props.errors)}</Text>*/}
-                {/*<Separator/>*/}
-                {/*<Text>{"Touched : " + JSON.stringify(props.touched)}</Text>*/}
-                {/*<Separator/>*/}
-                {/*<Text>{"Values : " + JSON.stringify(props.values)}</Text>*/}
-                {/*<Separator/>*/}
-                <Button styles={styles.submit} onPress={props.handleSubmit} title={translate('Submit')}/>
-              </View>
-            )}
-          </Formik>
-        </ScrollView>
-      </SafeAreaView>
-    )
-  }
+                                {/*<Text>{"Errors : " + JSON.stringify(props.errors)}</Text>*/}
+                                {/*<Separator/>*/}
+                                {/*<Text>{"Touched : " + JSON.stringify(props.touched)}</Text>*/}
+                                {/*<Separator/>*/}
+                                {/*<Text>{"Values : " + JSON.stringify(props.values)}</Text>*/}
+                                {/*<Separator/>*/}
+                                <Button styles={styles.submit} onPress={props.handleSubmit}
+                                        title={translate('Submit')}/>
+                            </View>
+                        )}
+                    </Formik>
+                </ScrollView>
+            </SafeAreaView>
+        )
+    }
 }
 
