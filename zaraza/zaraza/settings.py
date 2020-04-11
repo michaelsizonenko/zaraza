8ced5f65bf043c1f47c63f9c9b4307e32bbe1a1b
michaelsizonenko <michael.sizonenko.17@gmail.com>
2020-04-11 17:56:03 +0300

    debug sql

--- zaraza/zaraza/settings.py
+++ zaraza/zaraza/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '27)%42j#$tc#w-&23q_ep0a1a_m=rrtz)6##a4kcx^7tkhgeag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",'10.0.2.2'
@@ -145,6 +145,12 @@ LOGGING = {
         'handlers': ['console'],
         'level': 'DEBUG',
     },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
 }
 
 SALT = "Slava urkaine!"
